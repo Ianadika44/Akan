9fe35b95819925b93ddd7611b108432e0ffe62ef
Ianadika44 <adika19ian@gmail.com>
2020-02-14 12:19:17 +0300

    added variable names and days of the week

---
+++ script.js
@@ -0,0 +1,7 @@
+var  maleNames = ["Kwasi","Kwandwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+var daysOfWeek = ["sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+function giveName(){
+    
+}
